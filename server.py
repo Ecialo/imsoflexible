db93fc88229c01330123ca1ade42333069ebc885
Polik94 <polik94@fnbic.ru>
2016-05-30 17:46:52 +0300

    Parallel processing (with tornado queue)

--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 #! /usr/bin/python2
+# -*- coding: utf-8 -*-
 import tornado
 import tornado.ioloop
+from tornado import gen
+from tornado.queues import Queue
 import tornado.web
 import os, uuid
-
-from rolling_shutter import rolling_shutter
+from rolling_shutter import unblockable_rolling_shutter
 
 UPLOAD_DIR = "upload/"
 BUF_SIZE = 4096
 
 
+q = Queue()
+
+
+@gen.coroutine
+def watch_queue():
+    while True:
+        item = yield q.get()
+        try:
+            item.next()
+            yield gen.moment
+            q.put(item)
+        except StopIteration:
+            print "processing completed"
+        finally:
+            q.task_done()
+
+
 class Userform(tornado.web.RequestHandler):
+    @gen.coroutine
     def get(self):
         self.render("form.html")
 
 
 class Upload(tornado.web.RequestHandler):
+    @gen.coroutine
     def post(self):
 
         # TODO: Catch exceptions and delete bad sources
@@ -36,27 +57,33 @@ class Upload(tornado.web.RequestHandler):
         roughness = int(self.get_argument("roughness", 1))
         scale = float(self.get_argument("scale", 1))
 
-        rolling_shutter(path_src, result_filename=path_dst, roughness=roughness, scale=scale)
+        q.put(unblockable_rolling_shutter(path_src, result_filename=path_dst, roughness=roughness, scale=scale))
+        print "processing started..."
 
-        self.set_header('Content-Type', 'application/octet-stream')
-        self.set_header('Content-Disposition', 'attachment; filename=' + file_info['filename'])
-        with open(path_dst, 'r') as f:
-            while True:
-                data = f.read(BUF_SIZE)
-                if not data:
-                    break
-                self.write(data)
-        os.remove(path_src)
-        os.remove(path_dst)
+        # self.set_header('Content-Type', 'application/octet-stream')
+        # self.set_header('Content-Disposition', 'attachment; filename=' + file_info['filename'])
+        # with open(path_dst, 'r') as f:
+        #     while True:
+        #         data = f.read(BUF_SIZE)
+        #         if not data:
+        #             break
+        #         self.write(data)
+        # os.remove(path_src)
+        # os.remove(path_dst)
         self.finish()
 
 
-application = tornado.web.Application([
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8081)
+    parser.add_argument("-a", "--address", type=str, default='127.0.0.1')
+    args = parser.parse_args()
+
+    tornado.ioloop.IOLoop.instance().add_callback(watch_queue)
+    application = tornado.web.Application([
         (r"/", Userform),
         (r"/upload", Upload),
-        ], debug=True)
-
-
-if __name__ == "__main__":
-    application.listen(8081, address='127.0.0.1')
+    ], debug=True)
+    application.listen(args.port, address=args.address)
     tornado.ioloop.IOLoop.instance().start()
