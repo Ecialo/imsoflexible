d7fb988dabd9d70ef1924e97a87f39aa7a7e80e4
Polik94 <polik94@fnbic.ru>
2016-06-20 12:57:02 +0300

    Add ability to save non-square videos

--- rolling_shutter.py
+++ rolling_shutter.py
@@ -6,24 +6,16 @@ from collections import deque
 __author__ = 'ecialo'
 
 
-def cut(size):
-    def f(frame):
-        h, w = frame.shape
-        return frame[(h-size)/2:(h+size)/2, (w-size)/2:(w+size)/2]
-    return f
-
-
-def resize_and_cut(size, scale):
+def resize(scale):
     def f(frame):
         h, w = frame.shape
         h, w = int(h*scale), int(w*scale)
         new_frame = cv2.resize(frame, (w, h))
-        result = new_frame[(h-size)/2:(h+size)/2, (w-size)/2:(w+size)/2]
-        return result
+        return new_frame
     return f
 
 
-def make_rolling_frame(size, roughness):
+def make_rolling_frame(roughness):
     def f(frames):
         head = np.vstack([frame[roughness*i:roughness*(i+1), :] for i, frame in enumerate(frames)])
         try:
@@ -31,7 +23,7 @@ def make_rolling_frame(size, roughness):
         except ValueError:
             head = head.reshape((roughness, head.shape[0]))
             h, w = head.shape
-        tail = np.zeros((size - h, w), head.dtype)
+        tail = np.zeros((0, w), head.dtype)
         res = np.concatenate((head, tail))
         return res
     return f
@@ -45,13 +37,13 @@ def add_frame_to_frames(size):
     return f
 
 
-def configure_tools(frame, roughness, scale, maxsize, framerate, result_filename):
-    maxsize = maxsize or int(min(frame.shape)*scale)
-    stack_size = int(math.ceil(float(maxsize)/roughness))
+def configure_tools(frame, roughness, scale, framerate, result_filename):
+    h, w = frame.shape
+    stack_size = int(math.ceil(float(h)/roughness))
 
     appender = add_frame_to_frames(stack_size)
-    composer = make_rolling_frame(maxsize, roughness)
-    cutter = resize_and_cut(maxsize, scale)
+    composer = make_rolling_frame(roughness)
+    resizer = resize(scale)
 
     r, g, b = deque(), deque(), deque()
     result_video = cv2.VideoWriter(
@@ -61,13 +53,13 @@ def configure_tools(frame, roughness, scale, maxsize, framerate, result_filename
         # cv2.cv.FOURCC(*'MJPG'),
         # cv2.cv.FOURCC('P','I','M','1'),
         framerate,
-        (maxsize, maxsize),     # Ломается при неквадратных видео из-за бага в opencv
+        (int(w*scale), int(h*scale)),
     )
 
-    return maxsize, appender, composer, cutter, r, g, b, result_video
+    return appender, composer, resizer, r, g, b, result_video
 
 
-def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, framerate=25, result_filename=None):
+def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, framerate=25, result_filename=None):
     """
     Накладывает на видео эффект rolling shatter и сохраняет результат
 
@@ -84,13 +76,15 @@ def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, 
     :rtype:
     """
     video = cv2.VideoCapture(filename)
+    h = video.get(4)
+
     result_filename = result_filename or filename.split(".")[0] + "_lagged.avi"
     result_video = None
     r, g, b = None, None, None
     is_configured = False
     frame_counter = 0
 
-    cutter, appender, composer = None, None, None
+    resizer, appender, composer = None, None, None
 
     while True:
         is_success, frame = video.read()
@@ -100,11 +94,11 @@ def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, 
         if is_success:
             channels = cv2.split(frame)
             if not is_configured:
-                maxsize, appender, composer, cutter, r, g, b, result_video = configure_tools(
-                    channels[0], roughness, scale, maxsize, framerate, result_filename
+                appender, composer, resizer, r, g, b, result_video = configure_tools(
+                    channels[0], roughness, scale, framerate, result_filename
                 )
                 is_configured = True
-            new_b, new_g, new_r = map(cutter, channels)
+            new_b, new_g, new_r = map(resizer, channels)
             appender(b, new_b)
             appender(g, new_g)
             appender(r, new_r)
@@ -117,7 +111,7 @@ def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, 
             yield
         else:
             break
-    for i in xrange(int(math.ceil(float(maxsize)/roughness))):
+    for i in xrange(int(math.ceil(float(h)/roughness))):
         frame_counter += 1
         if frame_counter % 100 == 0:
             print "+"
@@ -135,8 +129,8 @@ def unblockable_rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, 
     result_video.release()
 
 
-def rolling_shutter(filename, roughness=1, scale=1.0, maxsize=None, framerate=25, result_filename=None):
-    reduce(lambda _, __: None, unblockable_rolling_shutter(filename, roughness, scale, maxsize, framerate, result_filename), None)
+def rolling_shutter(filename, roughness=1, scale=1.0, framerate=25, result_filename=None):
+    reduce(lambda _, __: None, unblockable_rolling_shutter(filename, roughness, scale, framerate, result_filename), None)
 
 
 if __name__ == '__main__':
